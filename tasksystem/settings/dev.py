aa2ebc16af6dabedf74bdcb8bb00c98b6ad5338f
Katy Williams <kawilliams@email.arizona.edu>
2024-09-02 20:47:25 -0400

    Add files via upload

---
+++ tasksystem/settings/dev.py
@@ -0,0 +1,31 @@
+from tasksystem.settings.base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# Option to lock down the task system
+# LOCK_TASKSELECTION = False
+if os.environ.get('LOCK_TASKS', 'false').lower() == 'true':
+    LOCK_TASKSELECTION = True
+else:
+    LOCK_TASKSELECTION = False
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'p#$hm-6n0^c6f6iuf0^iu0sla_()$)gt196m=cz&6_48_=68yq'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    }
+}
+
+CHANNEL_LAYERS['default'] = {
+    "BACKEND": "asgiref.inmemory.ChannelLayer",
+    "ROUTING": "taskselection.routing.channel_routing",
+}
+
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_REPLACE_HTTPS_REFERER = True
+CORS_ALLOW_CREDENTIALS = True
---
+++ tasksystem/settings/prod.py
@@ -0,0 +1,36 @@
+from tasksystem.settings.base import *
+import dj_database_url
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+# Option to lock down the task system
+if os.environ.get('LOCK_TASKS', 'false').lower() == 'true':
+    LOCK_TASKSELECTION = True
+else:
+    LOCK_TASKSELECTION = False
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'] = db_from_env
+
+ALLOWED_HOSTS = ['sv-task-system.herokuapp.com']
+
+# websockets
+CHANNEL_LAYERS['default'] = {
+    "BACKEND": "asgi_redis.RedisChannelLayer",
+    "CONFIG": {
+        "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+    },
+    "ROUTING": "taskselection.routing.channel_routing",
+}
+
+SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = os.environ.get('SECRET_KEY')
+
+# CORS settings
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ORIGIN_WHITELIST = ('vissv.org', '54.201.77.132', '159.127.104.69',
+                         'localhost')
+CORS_REPLACE_HTTPS_REFERER = True
+CORS_ALLOW_CREDENTIALS = True
