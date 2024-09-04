cc4e5a9f682c9e76ad60eec8279eef5c95492132
kawilliams <kawilliams@davidson.edu>
2024-09-04 17:35:42 -0400

    Changing CORS

--- tasksystem/settings/prod.py
+++ tasksystem/settings/prod.py
@@ -28,9 +28,9 @@ SECRET_KEY = os.environ['SECRET_KEY']
 #SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # CORS settings
-CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ORIGIN_WHITELIST = ('http://vissv.org', 'http://localhost', 'http://127.0.0.1')#'54.201.77.132', '159.127.104.69',
+#CORS_ORIGIN_ALLOW_ALL = True
+#CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['http://vissv.org', 'http://localhost', 'http://127.0.0.1', '54.201.77.132', '159.127.104.69']
                         
-CORS_REPLACE_HTTPS_REFERER = True
+#CORS_REPLACE_HTTPS_REFERER = True #deprecated
 CORS_ALLOW_CREDENTIALS = True
